cefc58f921934fdea3cabf28f4ef9dc8e72883cf
Evgeniy Filkin <jeka_krsk@mail.ru>
2024-08-22 12:52:42 +0300

    Commit v 1.0.3

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,16 @@
-import { Bowman, Swordsman, Magician, Daemon, Undead, Zombie } from './heroes';
+import Bowman from './classes/Bowman';
+import Daemon from './classes/Daemon';
+import Magician from './classes/Magician';
+import Swordsman from './classes/Swordsman';
+import Undead from './classes/Undead';
+import Zombie from './classes/Zombie';
 
 const heroes = [new Bowman('Bo'), new Swordsman('Sw'), new Magician('Ma'), new Daemon('Da'), new Undead('Un'), new Zombie('Zo')];
 
 heroes.forEach((hero, ind) => {
   console.log(ind, hero);
 });
+
+heroes[0].damage(133);
+heroes[1].levelUp();
+console.log(heroes[0], heroes[1]);
